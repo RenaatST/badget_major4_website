ab1da814d731a44e30762dd5de7c72187db0b28d
RenaatST <stuijkrenaat@hotmail.com>
2015-06-09 02:10:41 +0200

    front end renaat

--- view/Images/index.php
+++ view/Images/index.php
@@ -1,34 +1,8 @@
-<article class="intro">
-
-	<h1 class="hide">Samenwerking tussen Maes & Pukkelpop!</h1>
-	<p>mqlsdkjfqmlk mkljsdmlfkjq sdkfjkj aezkj  sdkjfqmslkdjfm jjkr ae sdlkjq   azelkrj amlkj qsd flkmsjd kjaz mkjazrez k mqlsdkjfqmlk mkljsdmlfkjq sdkfjkj aezkj  sdkjfqmslkdjfm jjkr ae sdlkjq   azelkrj amlkj qsd flkmsjd kjaz mkjazrez k mqlsdkjfqmlk mkljsdmlfkjq sdkfjkj aezkj  sdkjfqmslkdjfm jjkr ae sdlkjq   azelkrj amlkj qsd flkmsjd kjaz mkjazrez k mqlsdkjfqmlk mkljsdmlfkjq sdkfjkj aezkj  sdkjfqmslkdjfm jjkr ae sdlkjq   azelkrj amlkj qsd flkmsjd kjaz mkjazrez k mqlsdkjfqmlk mkljsdmlfkjq sdkfjkj aezkj  sdkjfqmslkdjfm jjkr ae sdlkjq </p>
-	
-</article>
-
-<article class="appstore">
-	
-	<img src="" alt="">
-	<a href="http://appstore.com/ocarina" target="_blank">Available in app store now!</a>
-
-</article>
-
-<article class="promofilm">
-	<h2>Promovideo Maes &amp; Pukkelpop</h2>
-
-	<video id="video" height="auto">
-	  <source src="./mov/mov.mp4" type="video/mp4">
-	  <source src="./mov/mov.ogv" type="video/ogg">
-		   Sorry, your browser doesn't support embedded videos, 
-	  but don't worry, you can <a href="./mov/mov.ogg">download it</a>
-	  and watch it with your favorite video player!
-	</video>
-
-
-</article>
-
 <article class="cups">
 
-	<h2>De cups!</h2>
+	<header>
+        <h1>De Cups</h1>
+    </header>
 
 	<ul>
 	<?php foreach ($bekers as $beker): ?>
@@ -45,40 +19,112 @@
 
 </article>
 
+
+<article class="stappenplan1">
 <article class="stappenplan">
 
-	<h2>Wat moet je doen?</h2>
+	<header>
+        <h1>Wat moet je doen?</h1>
+        <p>Volg deze stappen om een zelf samengestelde MAES CUP te winnen!</p>
+    </header>
+
+
 	
-	<div class="wrapper_stappenplan">
-		<section class="stap1">
-			<h2>stap 1</h2>
-			<figure>
-			  <img src="" alt="">
-			  <figcaption>qsdùmfjqmslkdjfmqlksjdf</figcaption>
-			</figure>
-		</section>
+	<ul>
+	<img src="./images/swirl.png" alt="" class="swirls">
+	<img src="./images/confetti.png" alt="" class="conf1"  data-1000="opacity:0;" data-1200="opacity:1;">
+	<img src="./images/confetti2.png" alt="" class="conf2" data-1000="opacity:0;" data-1200="opacity:1;">
+	 <li>
+	 	
+		<figure data-500="padding-left:-500px;margin-right:100px;" data-1200="margin-left:0;margin-right:0;">
+		  <img src="./images/beer.png" alt="" width="200" >
+		  <h2>Haal een pintje</h2>
+		  <figcaption>Koop een pintje aan één van vele bars op het festivalterrein!</figcaption>
+		</figure>
+	 </li>
+	 <li>
+		
+		<figure data-500="padding-left:-500px;margin-right:100px;" data-1200="margin-left:0;margin-right:0;">
+		  <img src="./images/pattern.png" alt="" width="200">
+		  <h2>Personaliseer</h2>
+		  <figcaption>Neem een foto en personaliseer jouw CUP!</figcaption>
+		</figure>
+	 </li>
+	 <li>
+		
+		<figure>
+		  <img src="./images/polaroid.png" alt="" width="200">
+		  <h2>Speel en win!</h2>
+		  <figcaption>Zoek je maat, en ga de strijd aan om jouw CUP te winnen!</figcaption>
+		</figure>
+	 </li>
+	</ul>
 
-		<section class="stap2">
-			<h2>stap 2</h2>
-			<figure>
-			  <img src="" alt="">
-			  <figcaption>qmsdlkjfmqlskjdfmqlksjd</figcaption>
-			</figure>
-		</section>
 
-		<section class="stap3">
-			<h2>stap 3</h2>
-			<figure>
-			  <img src="" alt="">
-			  <figcaption>qmsdlkfjmqsdlkjfqsdfqsd</figcaption>
-			</figure>
-		</section>
-	</div>
+</article>
+</article>
+
+
+
+
+
+
+<article class="promofilm">
+	<header>
+        <h1>Promovideo Maes &amp; Pukkelpop</h1>
+    </header>
+
+	<video id="video" height="auto" controls>
+	  <source src="./mov/mov.mp4" type="video/mp4">
+	  <source src="./mov/mov.ogv" type="video/ogg">
+		   Sorry, your browser doesn't support embedded videos, 
+	  but don't worry, you can <a href="./mov/mov.ogg">download it</a>
+	  and watch it with your favorite video player!
+	</video>
+
 
 </article>
 
+
+
+<article class="appstore">
+
+
+
+    <section class="imagesiphones">
+    	<img src="./images/iphone1.png" alt="screen1" class="screen1" width="200" height="400"/>
+		<img src="./images/iphone2.png" alt="screen2" class="screen2" width="250" height="400"/>
+		<img src="./images/iphone3.png" alt="screen3" class="screen3" width="250" height="400"/>
+    </section>
+
+
+	<section class="tekstappstore">
+		<header>
+        	<h1>Beschikbaar in de App Store</h1>
+	    </header>
+
+	    <p>Winnen of verliezen, meedoen is altijd even tof! Maak kans op een goodie bag van Maes. Download snel de app in de app store en wordt een maat van maes!</p>
+		</br>
+
+		<a href="http://appstore.com/ocarina" target="_blank"><img src="./images/buttonapp.png" alt="" width="100"><span class="hide">Available in app store now!</span>
+			
+		</a>
+
+	</section>
+	
+
+	
+	
+
+</article>
+
+
+
 <article class="prijs">
-	<h2>Prijs</h2>
+	<header>
+        <h1>Prijs</h1>
+    </header>
+
 	<figure>
 	  <img src="" alt="">
 	  <figcaption>qmsdlkfjmqsdlkjfqsdfqsd</figcaption>
--- view/layout.php
+++ view/layout.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link rel="stylesheet" href="css/screen.css">
     <title>MAES &amp; PUKKELPOP</title>
-    <link rel="stylesheet" href="css/bebas.css" type="text/css" charset="utf-8" />
-    <link rel="stylesheet" href="css/bebaslight.css" type="text/css" charset="utf-8" />
-    <link rel="stylesheet" href="css/ashley.css" type="text/css" charset="utf-8" />
+    <link rel="stylesheet" href="./css/bebas.css" type="text/css" charset="utf-8" />
+    <link rel="stylesheet" href="./css/bebaslight.css" type="text/css" charset="utf-8" />
+    <link rel="stylesheet" href="./css/ashley.css" type="text/css" charset="utf-8" />
+    
     <meta property="og:image" content="./images/bg.jpg"/>
     <link rel="image_src" href="./images/bg.jpg"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -35,11 +36,60 @@
         
 </head>
 <body>
-    <header>
-        <h1 class="logo">Maes &amp; Pukkelpop</h1>
-    </header>
 
-    <article id="container">
+    
+    <article class="intro">
+
+        <header>
+            <h1 class="hide">
+            Pukkelpop 2015
+            </h1>
+        </header>
+        
+        <nav>
+            <h1><span class="hide">Navigatie<span></h1>
+            <ul>
+                <li><a href="" title="">Cups</a></li>
+                <li><a href="" title="">Wedstrijd</a></li>
+                <li><a href="" title="">App Store</a></li>
+                <li><a href="" title="">Promofilm</a></li>
+                <li><a href="" title="">Prijzen</a></li>
+            </ul>
+        </nav>
+
+    
+        <header>
+            <h1 class="logo">
+            <span class="hide">Maes & Pukkelpop</span>
+                <a href="">
+                    <img data-0="width: 400px;height: 300px;" data-500="width: 500px;height: 400px;" src="./images/logo.png" alt="logo" width="400" height="300"/>
+                </a>
+            </h1>
+        </header>
+
+        <h2>Word maat van Maes en win jouw gepersonaliseerde beker!</h2>
+        <p>Iedereen wil deze prestigieuze MAES CUP winnen. Zoek je tegenstander en strijd om de beker! Sprint, balanceer, en maak vrienden. Iedereen wil deze prestigieuze MAES CUP winnen. </p>
+        
+            <img src="./images/mannekes.png" alt="mannekes" class="mannekess" width="100" height="70"/>
+
+        <form action="" method="post">
+            <fieldset>
+                <legend></legend>
+                <label for="txtzoek" class="zoeklabel">Zoek jouw beker</label>
+                <input type="text" name="txtzoek" required id="txtzoek" value="" />
+                <input type="submit" name="btnzoek" id="btnzoek" value="Zoek" />
+            </fieldset>
+        </form>
+
+
+
+    </article>
+
+    
+    <article class="container">
+        <header>
+            <h1 class="hide">Personaliseer jouw beker en win!</h1>
+        </header>
 
     <?php echo $content; ?>
 
@@ -47,6 +97,10 @@
 
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script>
-    <script src="js/script.dist.js" type="text/javascript"></script>
+    <script src="./js/script.dist.js" type="text/javascript"></script>
+    <script type="text/javascript" src="./js/skrollr.min.js"></script>
+    <script type="text/javascript">
+    var s = skrollr.init();
+    </script>
 </body>
 </html>
